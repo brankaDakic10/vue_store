43dc18ceaaf058ac7f4a7bab46111480b4cae479
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-06 19:59:54 +0200

    Added 5.Task

--- src/pages/AppProducts.vue
+++ src/pages/AppProducts.vue
@@ -1,10 +1,45 @@
 <template>
   <div class="container">
-         App Products
+      
+    <article>
+      <table class="table">
+          <thead class="header-table"> 
+                <tr>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Quantity</th>
+        
+                </tr>
+          </thead>
+          <tbody class="body-table">
+                <tr v-for="(product, key) in products" :key="key">
+                  <td>{{product.id}}</td>
+                  <td>{{product.title}}</td>
+                  <td>{{product.quantity}}</td>
+                  
+                </tr>
+          </tbody>
+      </table>
+  </article>
   </div>
 </template>
 <script>
+import {productService} from '../services/ProductService'
+
 export default {
-  
+  data(){
+    return{
+      products:productService.list()
+    }
+  }
 }
 </script>
+<style scoped>
+ .header-table{
+   background-color: rgb(176, 182, 187);
+ }
+
+ .body-table{
+   background-color: rgb(228, 233, 236);
+ }
+</style>
